c7eadfe6724a75f4f5034d3f9c5e0c14f5da306c
Matthew Roe <matthew@webfactory.ie>
2014-08-29 17:35:30 +0100

    more test stuff

--- temp.js
+++ temp.js
@@ -2,11 +2,18 @@
 
 var configObject;
 
-function getYoutubeRSS(configObject) {
-	var tempObj = configObject["youtube"];
-	var splitURL = configObject["youtube"]["rssURL"].split("{{}}");
-	for(var index = 0; i < splitURL.length; i++){
-
+function getRSS(configObject, feedType) {
+	var tempObj = configObject[feedType];
+	var finalURL = "";
+	if(configObject[feedType]["rssParams"] != undefined){
+		var splitURL = configObject[feedType]["rssURL"].split("{{}}");
+		for(var index = 0; i < splitURL.length; i++){
+			finalUrl += splitURL[index];
+			if(i < splitURL.length-1)
+				finalUrl += configObject[feedType]["rssParams"][i];
+		}
+	} else {
+		finalUrl = configObject[feedType]["rssURL"];
 	}
 	for(var key in tempObj){
 		if(tempObj[key])

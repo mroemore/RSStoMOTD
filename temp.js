a266e5fdcd0583637b2b46eb7fb290ecea56b4f3
Matthew Roe <matthew@webfactory.ie>
2014-08-29 17:16:16 +0100

    new config structure, some temp code in temp.js

---
+++ temp.js
@@ -0,0 +1,14 @@
+//working out new config muncher - NOM OM NOM ARHGHG
+
+var configObject;
+
+function getYoutubeRSS(configObject) {
+	var tempObj = configObject["youtube"];
+	var splitURL = configObject["youtube"]["rssURL"].split("{{}}");
+	for(var index = 0; i < splitURL.length; i++){
+
+	}
+	for(var key in tempObj){
+		if(tempObj[key])
+	}
+}
